827ce04f18a1e5afd815ce341fa902d258d3a847
Hendra Saputra <hendra.saputra@workday.com>
2022-04-19 11:53:33 +0100

    Capture risk and stats

--- index.mjs
+++ index.mjs
@@ -25,38 +25,71 @@ import fs from 'fs'
 
         const title = await page.$eval('.s-plain-card__title .s-plain-card__title-wrapper', title => title.textContent.trim())
         const author = await page.$eval('.s-plain-card__author a', author => author.textContent.trim())
-        const values = await page.$$eval(
+        const infoValues = await page.$$eval(
             '#left-panel.sidebar > div.s-list-info:nth-of-type(2) .s-list-info__item .s-list-info__label + .s-list-info__value',
             items => items.map(item => item.textContent.trim()))
 
-            var details = { signal, title, author }
-        values.forEach((value, index) => {
+        var details = { signal, title, author }
+        infoValues.forEach((info, index) => {
             switch (index) {
                 case 0:
-                    details.growth = value
+                    details.growth = info
                     break;
                 case 1:
-                    details.profit = value
+                    details.profit = info
                     break;
                 case 2:
-                    details.equity = value
+                    details.equity = info
                     break;
                 case 3:
-                    details.balance = value
+                    details.balance = info
                     break;
                 case 4:
-                    details["initial-deposit"] = value
+                    details["initial-deposit"] = info
                     break;
                 case 5:
-                    details.withdrawals = value
+                    details.withdrawals = info
                     break;
                 case 6:
-                    details.deposit = value
+                    details.deposit = info
                     break;
                 default:
                     break;
             }
         })
+
+        const riskValues = await page.$$eval(
+            'div#tab_content_risks div#risksTradeDataColumns div.header + div.s-data-columns__item + div.s-data-columns__item div.s-data-columns__value',
+            items => items.map(item => item.textContent.trim()))
+        riskValues.forEach((risk, index) => {
+            switch (index) {
+                case 0:
+                    details['drawdown-max-balance'] = risk
+                    break;
+                case 1:
+                    details['drawdown-max-equity'] = risk
+                    break;
+                default:
+                    break;
+            }
+        })
+
+        const statsValues = await page.$$eval(
+            'div#tab_content_stats div#tradeDataColumns div.s-data-columns > div.s-data-columns__column:first-child div.s-data-columns__item div.s-data-columns__value',
+            items => items.map(item => item.textContent.trim()))
+            statsValues.forEach((stat, index) => {
+            switch (index) {
+                case 1:
+                    details['profit-trades'] = stat
+                    break;
+                case 2:
+                    details['loss-trades'] = stat
+                    break;
+                default:
+                    break;
+            }
+        })
+
         result.push(details)
     }
 
